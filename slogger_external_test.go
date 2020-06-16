dd360d2fdc0bbac1c3c79c7419a64c9102c0f43e
stephen morris kinyori <kinyorimorris@gmail.com>
2020-06-16 16:10:44 -0700

    Replaced strings with constants and added test

--- slogger_external_test.go
+++ slogger_external_test.go
@@ -17,27 +17,24 @@ func TestDefaultLogLevel(t *testing.T) {
 		t.Errorf("The default log level should be INFO.  It is " + logLevel)
 	}
 }
-
 func TestSetLogLevelDebug(t *testing.T) {
 	slogger.SetLogLevelDebug()
 	if logLevel := slogger.GetLogLevel(); logLevel == slogger.InfoLogLevel {
 		t.Errorf("The default log level should be INFO.  It is " + logLevel)
 	}
 	if logLevel := slogger.GetLogLevel(); logLevel != slogger.DebugLogLevel {
-		t.Errorf("The default log level should be INFO.  It is " + logLevel)
+		t.Errorf("The default log level should be Debug.  It is " + logLevel)
 	}
 }
-
 func TestSetLogLevelInfo(t *testing.T) {
 	slogger.SetLogLevelInfo()
 	if logLevel := slogger.GetLogLevel(); logLevel != slogger.InfoLogLevel {
 		t.Errorf("The default log level should be INFO.  It is " + logLevel)
 	}
 	if logLevel := slogger.GetLogLevel(); logLevel == slogger.DebugLogLevel {
-		t.Errorf("The default log level should be INFO.  It is " + logLevel)
+		t.Errorf("The default log level should be Debug.  It is " + logLevel)
 	}
 }
-
 func TestDebugMessage(t *testing.T) {
 	slogger.SetLogLevelDebug()
 	r, w, _ := os.Pipe()
@@ -45,11 +42,10 @@ func TestDebugMessage(t *testing.T) {
 	slogger.Debug("Test Message")
 	w.Close()
 	out, _ := ioutil.ReadAll(r)
-	if ! strings.HasPrefix(string(out), "DEBUG") {
-		t.Errorf("The Debug message didn't was not found on StdOut.")
+	if ! strings.Contains(string(out), slogger.DebugLogLevel) {
+		t.Errorf("The Debug message was not found on StdOut.")
 	}
 }
-
 func TestDebugInfoMessage(t *testing.T) {
 	slogger.SetLogLevelDebug()
 	r, w, _ := os.Pipe()
@@ -57,19 +53,18 @@ func TestDebugInfoMessage(t *testing.T) {
 	slogger.Debug("Test Message")
 	w.Close()
 	out, _ := ioutil.ReadAll(r)
-	if ! strings.HasPrefix(string(out), "DEBUG") {
-		t.Errorf("The Debug message didn't was not found on StdOut.")
+	if ! strings.Contains(string(out), slogger.DebugLogLevel) {
+		t.Errorf("The Debug message was not found on StdOut.")
 	}
 	r, w, _ = os.Pipe()
 	os.Stdout = w
 	slogger.Info("Test Message")
 	w.Close()
 	out, _ = ioutil.ReadAll(r)
-	if ! strings.HasPrefix(string(out), "INFO") {
-		t.Errorf("The Info message didn't was not found on StdOut.")
+	if ! strings.Contains(string(out), slogger.InfoLogLevel) {
+		t.Errorf("The Info message was not found on StdOut.")
 	}
 }
-
 func TestDebugMethod(t *testing.T) {
 	slogger.SetLogLevelDebug()
 	r, w, _ := os.Pipe()
@@ -78,10 +73,26 @@ func TestDebugMethod(t *testing.T) {
 	w.Close()
 	out, _ := ioutil.ReadAll(r)
 	output := strings.TrimSuffix(string(out), "\n")
-	if ! strings.HasPrefix(output, "DEBUG") {
+	if ! strings.Contains(output, slogger.DebugLogLevel) {
+		t.Errorf("The Debug message was not found on StdOut.")
+	}
+	if ! strings.Contains(output, "TestDebugMethod") {
+		t.Errorf("The Debug message have the func name for this test in Stdout.")
+	}
+}
+func TestSetLogMessagePrefix(t *testing.T) {
+	slogger.SetLogLevelDebug()
+	slogger.SetLogMessagePrefix("slogger_test")
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	slogger.DebugMethod()
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	output := strings.TrimSuffix(string(out), "\n")
+	if ! strings.Contains(output, slogger.DebugLogLevel) && ! strings.Contains(output, "SLOGGER_TEST") {
 		t.Errorf("The Debug message didn't was not found on StdOut.")
 	}
-	if ! strings.HasSuffix(output, "TestDebugMethod") {
+	if ! strings.Contains(output, "TestDebugMethod") && ! strings.Contains(output, "SLOGGER_TEST") {
 		t.Errorf("The Debug message didn't have the func name for this test in Stdout.")
 	}
 }
