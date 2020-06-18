64ce2e6200d751820f553a6e37950f4120a4be1a
stephen morris kinyori <kinyorimorris@gmail.com>
2020-06-18 13:45:07 -0700

    Changed test from dbName to dbPassword

--- sConfigParams/sconfigparams_test.go
+++ sConfigParams/sconfigparams_test.go
@@ -2,8 +2,24 @@ package sConfigParams
 
 import (
 	"testing"
+
+	"gitlab.com/soteapps/packages/serror"
+	"gitlab.com/soteapps/packages/slogger"
 )
 
-func TestGetDBName(t *testing.T) {
+const (
+	tAWS       string = "staging"
+	dbPassword string = "password"
+)
+
+func TestGetDBPassword(t *testing.T) {
+	slogger.DebugMethod()
+
+	if _, found := GetDBPassword(tAWS); !found {
+		var p = make([]interface{}, 1)
+		p[0] = dbPassword
+		serror.GetSError(609999, p, serror.EmptyMap)
+		t.Errorf("TestGetDBPassword should have found the password: %v", serror.GetSError(609999, p, serror.EmptyMap).FmtErrMsg)
+	}
 
 }
