6c1eec98667f895c2009613760ba041c6563a330
stephen morris kinyori <kinyorimorris@gmail.com>
2020-06-18 13:44:48 -0700

    Added DB values and functions

--- sConfigParams/sconfigparams.go
+++ sConfigParams/sconfigparams.go
@@ -1,7 +1,6 @@
 package sConfigParams
 
 import (
-	"fmt"
 	"log"
 	"strings"
 
@@ -17,14 +16,20 @@ const (
 	demo        = "demo"
 	production  = "production"
 	// Variables
-	dbNameParam = "DATABASE_PASSWORD"
+	dbPasswordKey = "DATABASE_PASSWORD"
+	dbHostKey     = ""
+	dbUserKey     = ""
+	dbPortKey     = ""
+	dbNameKey     = ""
+	dbSSLModeKey  = ""
 )
 
 var (
-	defaultPath                 = "/sote/api"
-	dPath               *string = &defaultPath
-	setToTrue           bool    = true
-	sTrue               *bool   = &setToTrue
+	defaultPath                = "/sote/api/"
+	setToTrue           bool   = true
+	pTrue               *bool  = &setToTrue
+	maxResult           int64  = 10
+	pMaxResult          *int64 = &maxResult
 	sSSMParameterValues *ssm.GetParametersByPathOutput
 	awsService          *ssm.SSM
 )
@@ -41,37 +46,98 @@ func init() {
 	awsService = ssm.New(sess)
 }
 
-func init() {
+func initParameters(tAWS, key string) {
 	slogger.DebugMethod()
 
 	var (
-		ssmPath ssm.GetParametersByPathInput
-		err     error
+		ssmPath           ssm.GetParametersByPathInput
+		err               error
+		queryPath         string  = defaultPath + tAWS
+		pQueryPath        *string = &queryPath
+		queryPathWithKey  string  = defaultPath + tAWS + key
+		pQueryPathWithKey *string = &queryPathWithKey
 	)
-	ssmPath.Path = dPath
-	ssmPath.Recursive = sTrue
-	ssmPath.WithDecryption = sTrue
+	if len(key) == 0 {
+		ssmPath.Path = pQueryPath
+	} else {
+		ssmPath.Path = pQueryPathWithKey
+	}
+	ssmPath.Recursive = pTrue
+	ssmPath.WithDecryption = pTrue
+	ssmPath.MaxResults = pMaxResult
 	sSSMParameterValues, err = awsService.GetParametersByPath(&ssmPath)
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
-func GetDBName(tAWS string) (dbName string) {
+func GetDBPassword(tAWS string) (dbPassword string, found bool) {
 	slogger.DebugMethod()
 
-	found := false
+	initParameters(tAWS, dbPasswordKey)
+	dbPassword, found = search(tAWS, dbPasswordKey)
+
+	return
+}
+
+func GetDBHost(tAWS string) (dbHost string, found bool) {
+	slogger.DebugMethod()
+
+	initParameters(tAWS, dbHostKey)
+	dbHost, found = search(tAWS, dbHostKey)
+
+	return
+}
+
+func GetDBUser(tAWS string) (dbUser string, found bool) {
+	slogger.DebugMethod()
+
+	initParameters(tAWS, dbUserKey)
+	dbUser, found = search(tAWS, dbUserKey)
+
+	return
+}
+
+func GetDBPort(tAWS string) (dbPort string, found bool) {
+	slogger.DebugMethod()
+
+	initParameters(tAWS, dbPortKey)
+	dbPort, found = search(tAWS, dbPortKey)
+
+	return
+}
+
+func GetDBName(tAWS string) (dbName string, found bool) {
+	slogger.DebugMethod()
+
+	initParameters(tAWS, dbNameKey)
+	dbName, found = search(tAWS, dbNameKey)
+
+	return
+}
+
+func GetDBSSLMode(tAWS string) (dbSSLMode string, found bool) {
+	slogger.DebugMethod()
+
+	initParameters(tAWS, dbSSLModeKey)
+	dbSSLMode, found = search(tAWS, dbSSLModeKey)
+
+	return
+}
+
+func search(tAWS, key string) (value string, found bool) {
+	slogger.DebugMethod()
+
+	found = false
 	for _, v := range sSSMParameterValues.Parameters {
 		if strings.Contains(*v.Name, tAWS) {
-			if strings.Contains(*v.Name, dbNameParam) {
+			if strings.Contains(*v.Name, key) {
 				found = true
-				fmt.Println(v)
+				value = *v.Value
+				break
 			}
 		}
 	}
 
-	if !found {
-
-	}
 	return
 }
