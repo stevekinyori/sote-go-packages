800b1ef731f63a85b24f9b2eabb43e93049da7c3
stephen morris kinyori <kinyorimorris@gmail.com>
2020-07-07 17:14:58 -0700

    Fixed messages and added tests for conn and returned data

--- sDatabase/stable_test.go
+++ sDatabase/stable_test.go
@@ -5,19 +5,39 @@ import (
 )
 
 func TestGetTables(t *testing.T) {
-	soteErr := GetConnection("single", "sote_development", "sote", "password", "localhost", "disable", 5442, 3)
-	if soteErr.ErrCode != nil {
-		t.Errorf("Get Connection Failed: Should have returned a pointer to the single database connection")
+	if _, soteErr := getTables("sote"); soteErr.ErrCode != 602999 {
+		t.Errorf("Get Tables Failed: Expected error code of 602999")
 		t.Fail()
 	}
 
-	getTables("sote")
+	soteErr := GetConnection("single", "sote_development", "sote", "password", "localhost", "disable", 5442, 3)
+	if soteErr.ErrCode != nil {
+		t.Errorf("Get Connection Failed: Please Investigate")
+		t.Fail()
+	}
+
+	var tableList []string
+	if tableList, soteErr = getTables("sote"); soteErr.ErrCode != nil {
+		t.Errorf("Get Tables Failed: Expected error code to be nil")
+		t.Fail()
+	}
+	if len(tableList) == 0 {
+		t.Errorf("Get Tables Failed: Expected at least one table name to be returned")
+		t.Fail()
+	}
 
 	soteErr = GetConnection("pool", "sote_development", "sote", "password", "localhost", "disable", 5442, 3)
 	if soteErr.ErrCode != nil {
-		t.Errorf("Get Connection Failed: Should have returned a pointer to the single database connection")
+		t.Errorf("Get Connection Failed: Please Investigate")
 		t.Fail()
 	}
 
-	getTables("sote")
+	if tableList, soteErr = getTables("sote"); soteErr.ErrCode != nil {
+		t.Errorf("Get Tables Failed: Expected error code to be nil")
+		t.Fail()
+	}
+	if len(tableList) == 0 {
+		t.Errorf("Get Tables Failed: Expected at least one table name to be returned")
+		t.Fail()
+	}
 }
