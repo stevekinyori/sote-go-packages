7a9c98cc473c542e440d18aa395cc236e87037b0
stephen morris kinyori <kinyorimorris@gmail.com>
2020-07-09 16:09:10 -0700

    Added sDatabase to test.sh

--- test.sh
+++ test.sh
@@ -3,17 +3,20 @@
 # This is a testing script for the seeding utility
 #
 # The following will generate coverage.out files and new packages should be added.
-rm sLogger/coverage.out> /dev/null; go test sLogger/* -coverprofile sLogger/coverage.out
-rm sError/coverage.out> /dev/null; go test sError/* -coverprofile sError/coverage.out
+rm sLogger/coverage.out 2> /dev/null; go test sLogger/* -coverprofile sLogger/coverage.out
+rm sError/coverage.out 2> /dev/null; go test sError/* -coverprofile sError/coverage.out
+rm sDatabase/coverage.out 2> /dev/null; go test sDatabase/* -coverprofile sDatabase/coverage.out
 
 # The following will run tests that use the packages as an external package or program.
 # There is no coverage because the tests are outside the directory with the source file.
 go test sError_external_test.go
 go test sLogger_external_test.go
+go test sDatabase_external_test.go
 
 # To output the results from the -coverprofile, using the following command
 go tool cover -func=sError/coverage.out > coverage_review.out
 go tool cover -func=sLogger/coverage.out >> coverage_review.out
+go tool cover -func=sDatabase/coverage.out >> coverage_review.out
 
 # Review the coverage totals for 70% compliance
 grep '^total' coverage_review.out | awk '/[0-6][0-9]./ { print "FAILED: Coverage must be 70% or higher" }'
