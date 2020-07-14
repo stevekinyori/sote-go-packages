2b2dc9144323ed393a3418f8c2fba0f38691a628
stephen morris kinyori <kinyorimorris@gmail.com>
2020-07-13 17:22:13 -0700

    Number of changes
    fix const declaration to uppercase
    remove datatype for pointers
    added comments
    added validation for environments to initParameters
    added logic to add filter to GetParametersByPath
    added logic to pull value from returned pointer
    removed search
    added needed tests for functions

--- sConfigParams/sconfigparams.go
+++ sConfigParams/sconfigparams.go
@@ -1,3 +1,11 @@
+/*
+This will query System Manager Parameter store for the path and (optional) key provided.
+
+RESTRICTIONS:
+    * Program must have access to a .aws/credentials file in the default locate.
+    * This will only access system parameters that start with '/sote' (ROOTPATH).
+    * You can only request a single key per call
+*/
 package sConfigParams
 
 import (
@@ -6,35 +14,39 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ssm"
+	"gitlab.com/soteapps/packages/v2020/sError"
 	"gitlab.com/soteapps/packages/v2020/sLogger"
 )
 
 const (
 	// Environments
-	staging     = "staging"
-	development = "development"
-	demo        = "demo"
-	production  = "production"
-	// Variables
-	dbPasswordKey = "DATABASE_PASSWORD"
-	dbHostKey     = ""
-	dbUserKey     = ""
-	dbPortKey     = ""
-	dbNameKey     = ""
-	dbSSLModeKey  = ""
-	noKey         = ""
+	STAGING     = "staging"
+	DEVELOPMENT = "development"
+	DEMO        = "demo"
+	PRODUCTION  = "production"
+	//
+	DBPASSWORDKEY = "DATABASE_PASSWORD"
+	DBHOSTKEY     = ""
+	DBUSERKEY     = ""
+	DBPORTKEY     = ""
+	DBNAMEKEY     = ""
+	DBSSLMODEKEY  = ""
+	NOKEY         = ""
+	ROOTPATH      = "/sote"
 )
 
 var (
-	defaultPath                = "/sote/api/"
-	setToTrue           bool   = true
-	pTrue               *bool  = &setToTrue
-	maxResult           int64  = 10
-	pMaxResult          *int64 = &maxResult
-	sSSMParameterValues *ssm.GetParametersByPathOutput
-	awsService          *ssm.SSM
+	awsService     *ssm.SSM
+	setToTrue            = true       // This can not be a constant because we need a pointer.
+	pTrue                = &setToTrue // pointer to the setToTrue variable
+	maxResult      int64 = 10
+	pMaxResult           = &maxResult
+	pSSMPathOutput *ssm.GetParametersByPathOutput
 )
 
+/*
+This will establish a session using the default .aws location
+*/
 func init() {
 	sLogger.DebugMethod()
 
@@ -47,108 +59,154 @@ func init() {
 	awsService = ssm.New(sess)
 }
 
-func initParameters(tAWS, key string) {
+/*
+This will build the query path based on the ROOTPATH, Application and Environment.  The
+Environment is validated against 'development', 'staging', 'demo' and 'production' (case sensitive values)
+*/
+func initParameters(tApplication, tEnvironment, key string) (soteErr sError.SoteError) {
 	sLogger.DebugMethod()
 
-	var (
-		ssmPath           ssm.GetParametersByPathInput
-		err               error
-		queryPath         string  = defaultPath + tAWS
-		pQueryPath        *string = &queryPath
-		queryPathWithKey  string  = defaultPath + tAWS + key
-		pQueryPathWithKey *string = &queryPathWithKey
-	)
-	if len(key) == 0 {
-		ssmPath.Path = pQueryPath
-	} else {
-		ssmPath.Path = pQueryPathWithKey
-	}
-	ssmPath.Recursive = pTrue
-	ssmPath.WithDecryption = pTrue
-	ssmPath.MaxResults = pMaxResult
-	sSSMParameterValues, err = awsService.GetParametersByPath(&ssmPath)
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
-
-func GetParameters(tAWS string) (parameters []*ssm.Parameter, found bool) {
-	sLogger.DebugMethod()
-
-	initParameters(tAWS, noKey)
-	if len(sSSMParameterValues.Parameters) > 0 {
-		parameters = sSSMParameterValues.Parameters
+	switch tEnvironment {
+	case DEVELOPMENT:
+	case STAGING:
+	case DEMO:
+	case PRODUCTION:
+	default:
+		soteErr = sError.GetSError(601010, sError.BuildParams([]string{tEnvironment}), sError.EmptyMap)
 	}
 
-	return
-}
-
-func GetDBPassword(tAWS string) (dbPassword string, found bool) {
-	sLogger.DebugMethod()
-
-	initParameters(tAWS, dbPasswordKey)
-	dbPassword, found = search(tAWS, dbPasswordKey)
-
-	return
-}
-
-func GetDBHost(tAWS string) (dbHost string, found bool) {
-	sLogger.DebugMethod()
-
-	initParameters(tAWS, dbHostKey)
-	dbHost, found = search(tAWS, dbHostKey)
-
-	return
-}
-
-func GetDBUser(tAWS string) (dbUser string, found bool) {
-	sLogger.DebugMethod()
-
-	initParameters(tAWS, dbUserKey)
-	dbUser, found = search(tAWS, dbUserKey)
-
-	return
-}
-
-func GetDBPort(tAWS string) (dbPort string, found bool) {
-	sLogger.DebugMethod()
-
-	initParameters(tAWS, dbPortKey)
-	dbPort, found = search(tAWS, dbPortKey)
-
-	return
-}
-
-func GetDBName(tAWS string) (dbName string, found bool) {
-	sLogger.DebugMethod()
-
-	initParameters(tAWS, dbNameKey)
-	dbName, found = search(tAWS, dbNameKey)
-
-	return
-}
-
-func GetDBSSLMode(tAWS string) (dbSSLMode string, found bool) {
-	sLogger.DebugMethod()
-
-	initParameters(tAWS, dbSSLModeKey)
-	dbSSLMode, found = search(tAWS, dbSSLModeKey)
-
-	return
-}
-
-func search(tAWS, key string) (value string, found bool) {
-	sLogger.DebugMethod()
-
-	found = false
-	for _, v := range sSSMParameterValues.Parameters {
-		if strings.Contains(*v.Name, tAWS) {
-			if strings.Contains(*v.Name, key) {
-				found = true
-				value = *v.Value
-				break
+	if soteErr.ErrCode == nil {
+		var (
+			err          error
+			ssmPathInput ssm.GetParametersByPathInput
+			path         = ROOTPATH + "/" + tApplication + "/" + tEnvironment
+			pathWithKey  = path + "/" + key
+			filter       ssm.ParameterStringFilter
+			pFilter      []*ssm.ParameterStringFilter
+			pKeys        []*string
+		)
+		ssmPathInput.SetPath(path)
+		ssmPathInput.Recursive = pTrue
+		ssmPathInput.WithDecryption = pTrue
+		ssmPathInput.MaxResults = pMaxResult
+		if len(key) > 0 {
+			pKeys = append(pKeys, &key)
+			filter.SetKey("string")
+			filter.SetOption("string")
+			filter.SetValues(pKeys)
+			ssmPathInput.SetParameterFilters(pFilter)
+		}
+		if pSSMPathOutput, err = awsService.GetParametersByPath(&ssmPathInput); len(pSSMPathOutput.String()) == 0 {
+			if len(key) > 0 {
+				soteErr = sError.GetSError(109999, sError.BuildParams([]string{pathWithKey}), sError.EmptyMap)
+			} else {
+				soteErr = sError.GetSError(109999, sError.BuildParams([]string{path}), sError.EmptyMap)
 			}
 		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	return
+}
+
+/*
+This will retrieve the parameters that are in the AWS System Manager service for the ROOTPATH and the supplied
+application and environment.  AWS limits the maximum number of parameters to 10 in a single query.  sconfigparams
+doesn't support pulling more than the first 10 parameters.
+*/
+func GetParameters(tApplication, tEnvironment string) (parameters map[string]interface{}, soteErr sError.SoteError) {
+	sLogger.DebugMethod()
+
+	parameters = make(map[string]interface{})
+	if soteErr = initParameters(tApplication, strings.ToLower(tEnvironment), NOKEY); soteErr.ErrCode == nil {
+		for _, pParameter := range pSSMPathOutput.Parameters {
+			parameters[*pParameter.Name] = *pParameter.Value
+		}
+	}
+
+	return
+}
+
+/*
+This will retrieve the database password parameter that is in AWS System Manager service for the ROOTPATH and
+application.
+*/
+func GetDBPassword(tApplication, tEnvironment string) (dbPassword string, soteErr sError.SoteError) {
+	sLogger.DebugMethod()
+
+	if soteErr = initParameters(tApplication, strings.ToLower(tEnvironment), DBPASSWORDKEY); soteErr.ErrCode == nil {
+		dbPassword = *pSSMPathOutput.Parameters[0].Value
+	}
+
+	return
+}
+
+/*
+This will retrieve the database host parameter that is in AWS System Manager service for the ROOTPATH and
+application.
+*/
+func GetDBHost(tApplication, tEnvironment string) (dbHost string, soteErr sError.SoteError) {
+	sLogger.DebugMethod()
+
+	if soteErr = initParameters(tApplication, strings.ToLower(tEnvironment), DBHOSTKEY); soteErr.ErrCode == nil {
+		dbHost = *pSSMPathOutput.Parameters[0].Value
+	}
+	return
+}
+
+/*
+This will retrieve the database user parameter that is in AWS System Manager service for the ROOTPATH and
+application.
+*/
+func GetDBUser(tApplication, tEnvironment string) (dbUser string, soteErr sError.SoteError) {
+	sLogger.DebugMethod()
+
+	if soteErr = initParameters(tApplication, strings.ToLower(tEnvironment), DBUSERKEY); soteErr.ErrCode == nil {
+		dbUser = *pSSMPathOutput.Parameters[0].Value
+	}
+
+	return
+}
+
+/*
+This will retrieve the database port parameter that is in AWS System Manager service for the ROOTPATH and
+application.
+*/
+func GetDBPort(tApplication, tEnvironment string) (dbPort string, soteErr sError.SoteError) {
+	sLogger.DebugMethod()
+
+	if soteErr = initParameters(tApplication, strings.ToLower(tEnvironment), DBPORTKEY); soteErr.ErrCode == nil {
+		dbPort = *pSSMPathOutput.Parameters[0].Value
+	}
+
+	return
+}
+
+/*
+This will retrieve the database name parameter that is in AWS System Manager service for the ROOTPATH and
+application.
+*/
+func GetDBName(tApplication, tEnvironment string) (dbName string, soteErr sError.SoteError) {
+	sLogger.DebugMethod()
+
+	if soteErr = initParameters(tApplication, strings.ToLower(tEnvironment), DBNAMEKEY); soteErr.ErrCode == nil {
+		dbName = *pSSMPathOutput.Parameters[0].Value
+	}
+
+	return
+}
+
+/*
+This will retrieve the database SSL mode parameter that is in AWS System Manager service for the ROOTPATH and
+application.
+*/
+func GetDBSSLMode(tApplication, tEnvironment string) (dbSSLMode string, soteErr sError.SoteError) {
+	sLogger.DebugMethod()
+
+	if soteErr = initParameters(tApplication, tEnvironment, DBSSLMODEKEY); soteErr.ErrCode == nil {
+		dbSSLMode = *pSSMPathOutput.Parameters[0].Value
 	}
 
 	return
--- sConfigParams/sconfigparams_test.go
+++ sConfigParams/sconfigparams_test.go
@@ -3,26 +3,51 @@ package sConfigParams
 import (
 	"testing"
 
-	"gitlab.com/soteapps/packages/v2020/sLogger"
+	"gitlab.com/soteapps/packages/v2020/sError"
 )
 
 const (
-	tAWS       string = "staging"
-	dbPassword string = "password"
+	// Application values
+	API  string = "api"
 )
 
+func TestGetParameters(t *testing.T) {
+	parameters := make(map[string]interface{})
+	var soteErr sError.SoteError
+	if parameters, soteErr = GetParameters(API, DEVELOPMENT); soteErr.ErrCode != nil {
+		t.Errorf("GetParameters failed: Expected soteErr to be nil: %v", soteErr.ErrCode)
+	}
+	if len(parameters) == 0 {
+		t.Error("GetParameters failed: Expected parameters to have at least one entry")
+	}
+}
 func TestGetDBPassword(t *testing.T) {
-	sLogger.DebugMethod()
-
-	if _, found := GetDBPassword(tAWS); !found {
-		var p = make([]interface{}, 1)
-		p[0] = dbPassword
-		t.Errorf("TestGetDBPassword should have found the password: %v", "asdf")
+	if _, soteErr := GetDBPassword(API, DEVELOPMENT); soteErr.ErrCode != nil {
+		t.Errorf("TestGetDBPassword failed: Expected soteErr to be nil: %v", soteErr.ErrCode)
 	}
-	if _, found := GetDBPassword(tAWS); !found {
-		var p = make([]interface{}, 1)
-		p[0] = dbPassword
-		t.Errorf("TestGetDBPassword should have found the password: %v", "asdf")
+}
+func TestGetDBName(t *testing.T) {
+	if _, soteErr := GetDBName(API, DEVELOPMENT); soteErr.ErrCode != nil {
+		t.Errorf("GetDBName failed: Expected soteErr to be nil: %v", soteErr.ErrCode)
 	}
-
+}
+func TestGetDBHost(t *testing.T) {
+	if _, soteErr := GetDBHost(API, DEVELOPMENT); soteErr.ErrCode != nil {
+		t.Errorf("GetDBHost failed: Expected soteErr to be nil: %v", soteErr.ErrCode)
+	}
+}
+func TestGetDBPort(t *testing.T) {
+	if _, soteErr := GetDBPort(API, DEVELOPMENT); soteErr.ErrCode != nil {
+		t.Errorf("GetDBPort failed: Expected soteErr to be nil: %v", soteErr.ErrCode)
+	}
+}
+func TestGetDBSSLMode(t *testing.T) {
+	if _, soteErr := GetDBSSLMode(API, DEVELOPMENT); soteErr.ErrCode != nil {
+		t.Errorf("GetDBSSLMode failed: Expected soteErr to be nil: %v", soteErr.ErrCode)
+	}
+}
+func TestGetDBUser(t *testing.T) {
+	if _, soteErr := GetDBUser(API, DEVELOPMENT); soteErr.ErrCode != nil {
+		t.Errorf("GetDBUser failed: Expected soteErr to be nil: %v", soteErr.ErrCode)
+	}
 }
