aaa9944bc73e8e0def02235f8ee78795519d4f43
stephen morris kinyori <kinyorimorris@gmail.com>
2020-07-28 15:01:08 -0700

    Moved tests to AWS and Environment based testing

--- sDatabase/sconnection_test.go
+++ sDatabase/sconnection_test.go
@@ -2,6 +2,16 @@ package sDatabase
 
 import (
 	"testing"
+
+	"gitlab.com/soteapps/packages/v2020/sConfigParams"
+	"gitlab.com/soteapps/packages/v2020/sError"
+)
+
+var (
+	tConnInfo                                                ConnInfo
+	awsRegion, dbName, dbUser, dbPassword, dbHost, dbSSLMode string
+	dbPort                                                   int
+	soteErr                                                  sError.SoteError
 )
 
 func TestSetConnectionValue(t *testing.T) {
@@ -24,14 +34,19 @@ func TestSetConnectionValues(t *testing.T) {
 	}
 }
 func TestVerifyConnection(t *testing.T) {
-	var tConnInfo ConnInfo
-	soteErr := VerifyConnection(tConnInfo)
+
+	soteErr = VerifyConnection(tConnInfo)
 	if soteErr.ErrCode != 602999 {
 		t.Errorf("VerifyConnection Failed: Expected 602999 error code.")
 		t.Fail()
 	}
 
-	tConnInfo, soteErr = GetConnection("sote_development", "sote", "password", "localhost", "disable", 5442, 3)
+	if soteErr = getAWSParams(); soteErr.ErrCode != nil {
+		t.Errorf("getAWSParams Failed: Expected error code to be nil.")
+		t.Fatal()
+	}
+
+	tConnInfo, soteErr = GetConnection(dbName, dbUser, dbPassword, dbHost, dbSSLMode, dbPort, 3)
 	if soteErr.ErrCode != nil {
 		t.Errorf("setConnectionValues Failed: Expected a nil error code.")
 		t.Fail()
@@ -43,28 +58,20 @@ func TestVerifyConnection(t *testing.T) {
 		t.Fail()
 	}
 }
-func TestGetConnection(t *testing.T) {
-	_, soteErr := GetConnection("sote_development", "sote", "password", "localhost", "disable", 5442, 3)
-	if soteErr.ErrCode != nil {
-		t.Errorf("GetConnection Failed: Please Investigate")
-		t.Fail()
+func TestToJSONString(t *testing.T) {
+	if soteErr = getAWSParams(); soteErr.ErrCode != nil {
+		t.Errorf("getAWSParams Failed: Expected error code to be nil.")
+		t.Fatal()
 	}
 
-	_, soteErr = setConnectionValues("dbName", "User", "Password", "Host", "disable", 1, 1)
-	if soteErr.ErrCode != nil {
-		t.Errorf("setConnectionValues Failed: Expected a nil error code.")
-		t.Fail()
-	}
-}
-func TestToJSONString(t *testing.T) {
-	dbConnInfo, soteErr := GetConnection("sote_development", "sote", "password", "localhost", "disable", 5442, 3)
+	tConnInfo, soteErr = GetConnection(dbName, dbUser, dbPassword, dbHost, dbSSLMode, dbPort, 3)
 	if soteErr.ErrCode != nil {
 		t.Errorf("GetConnection Failed: Please Investigate")
 		t.Fail()
 	}
 
 	var dbConnJSONString string
-	if dbConnJSONString, soteErr = ToJSONString(dbConnInfo.DSConnValues); soteErr.ErrCode != nil {
+	if dbConnJSONString, soteErr = ToJSONString(tConnInfo.DSConnValues); soteErr.ErrCode != nil {
 		t.Errorf("ToJSONString Failed: Please Investigate")
 		t.Fail()
 	}
@@ -73,4 +80,27 @@ func TestToJSONString(t *testing.T) {
 		t.Errorf("ToJSONString Failed: Please Investigate")
 		t.Fail()
 	}
+}
+func getAWSParams() (soteErr sError.SoteError) {
+	awsRegion, soteErr = sConfigParams.GetEnvironmentAWSRegion()
+	if soteErr.ErrCode == nil {
+		dbName, soteErr = sConfigParams.GetDBName("API", awsRegion)
+		if soteErr.ErrCode == nil {
+			dbUser, soteErr = sConfigParams.GetDBUser("API", awsRegion)
+			if soteErr.ErrCode == nil {
+				dbPassword, soteErr = sConfigParams.GetDBPassword("API", awsRegion)
+				if soteErr.ErrCode == nil {
+					dbHost, soteErr = sConfigParams.GetDBHost("API", awsRegion)
+					if soteErr.ErrCode == nil {
+						dbSSLMode, soteErr = sConfigParams.GetDBSSLMode("API", awsRegion)
+						if soteErr.ErrCode == nil {
+							dbPort, soteErr = sConfigParams.GetDBPort("API", awsRegion)
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return
 }
