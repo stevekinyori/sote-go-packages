e9b2bd3540c4fd7a38c87f3ede7e91a265a19bfc
stephen morris kinyori <kinyorimorris@gmail.com>
2020-08-17 13:55:27 -0700

    Changed test failed message

--- sDatabase/stableinfo_test.go
+++ sDatabase/stableinfo_test.go
@@ -7,7 +7,7 @@ import (
 func TestGetTables(t *testing.T) {
 	var tConnInfo ConnInfo
 	if _, soteErr := GetTableList("sote", tConnInfo); soteErr.ErrCode != 602999 {
-		t.Errorf("Get Tables Failed: Expected error code of 602999")
+		t.Errorf("GetTableList Failed: Expected error code of 602999")
 		t.Fail()
 	}
 
@@ -18,18 +18,18 @@ func TestGetTables(t *testing.T) {
 
 	tConnInfo, soteErr = GetConnection(DBName, DBUser, DBPassword, DBHost, DBSSLMode, DBPort, 3)
 	if soteErr.ErrCode != nil {
-		t.Errorf("Get Connection Failed: Please Investigate")
+		t.Errorf("GetConnection Failed: Please Investigate")
 		t.Fail()
 	}
 
 	var tableList []string
 	if tableList, soteErr = GetTableList("sote", tConnInfo); soteErr.ErrCode != nil {
-		t.Errorf("Get Tables Failed: Expected error code to be nil")
+		t.Errorf("GetTableList Failed: Expected error code to be nil")
 		t.Fail()
 	}
 
 	if len(tableList) == 0 {
-		t.Errorf("Get Tables Failed: Expected at least one table name to be returned")
+		t.Errorf("GetTableList Failed: Expected at least one table name to be returned")
 		t.Fail()
 	}
 }
