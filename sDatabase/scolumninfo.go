ab3eea1e48515e35ddb3783fab1b04166191e851
stephen morris kinyori <kinyorimorris@gmail.com>
2020-08-17 14:45:33 -0700

    Modified to pull needed column information

--- sDatabase/scolumninfo.go
+++ sDatabase/scolumninfo.go
@@ -10,12 +10,20 @@ import (
 	"gitlab.com/soteapps/packages/v2020/sLogger"
 )
 
-// This function gets a list of tables for the supplied schema.
-func GetColumnInfo(schemaName, tableName string, tConnInfo ConnInfo) (columnList []string, soteErr sError.SoteError) {
+type SColumnInfo struct {
+	// Col was added to the name to prevent conflicts
+	colName     string
+	colDefault  string
+	colNullable string
+	colDataType string
+}
+
+// This function gets column information for the supplied schema and table.
+func GetColumnInfo(schemaName, tableName string, tConnInfo ConnInfo) (tableColumnInfo []SColumnInfo, soteErr sError.SoteError) {
 	sLogger.DebugMethod()
 
 	if soteErr = VerifyConnection(tConnInfo); soteErr.ErrCode == nil {
-		qStmt := "SELECT table_name FROM information_schema.columns WHERE table_schema = $1 AND table_name = $2;"
+		qStmt := "SELECT column_name, column_default, is_nullable, data_type FROM information_schema.columns WHERE table_schema = $1 AND table_name = $2;"
 
 		var colRows pgx.Rows
 		var err error
@@ -25,13 +33,21 @@ func GetColumnInfo(schemaName, tableName string, tConnInfo ConnInfo) (columnList
 		}
 
 		var columnRow []interface{}
+		var tRowInfo SColumnInfo
 		for colRows.Next() {
 			columnRow, err = colRows.Values()
 			if err != nil {
 				log.Fatalln(err)
 			}
-			columnList = append(columnList, columnRow[0].(string))
-
+			tRowInfo.colName = columnRow[0].(string)
+			if columnRow[1] == nil {
+				tRowInfo.colDefault = ""
+			} else {
+				tRowInfo.colDefault = columnRow[1].(string)
+			}
+			tRowInfo.colNullable = columnRow[2].(string)
+			tRowInfo.colDataType = columnRow[3].(string)
+			tableColumnInfo = append(tableColumnInfo, tRowInfo)
 		}
 		defer colRows.Close()
 	}
