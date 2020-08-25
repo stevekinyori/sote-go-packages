560a93f697baee4ebabbb40a69d1e57c3d68418c
stephen morris kinyori <kinyorimorris@gmail.com>
2020-08-25 14:12:54 -0700

    Build out of test structures and data

--- db/migration/testdbstructures.sql
+++ db/migration/testdbstructures.sql
@@ -35,46 +35,32 @@ ALTER TABLE sotetest.referencetable
     OWNER TO sote;
 
 INSERT INTO sotetest.referencetable (reference_name, reference_name_display, created_by_date, created_by_requestor_username, updated_by_date, updated_by_requestor_username)
-VALUES ('COMPANY_NAME', 'Company Name', '2020-07-27', 'HARVESTER', NULL, NULL);
+VALUES ('TEST_COMPANY_NAME', 'TEST Company Name', '2020-07-27', 'HARVESTER', NULL, NULL);
 INSERT INTO sotetest.referencetable (reference_name, reference_name_display, created_by_date, created_by_requestor_username, updated_by_date, updated_by_requestor_username)
-VALUES ('PORT_OF_ENTRY', 'Port of Entry', '2020-07-27', 'HARVESTER', '2020-07-27', 'HARVESTER');
+VALUES ('TEST_PORT_OF_ENTRY', 'TEST Port of Entry', '2020-07-27', 'HARVESTER', '2020-07-27', 'HARVESTER');
 INSERT INTO sotetest.referencetable (reference_name, reference_name_display, created_by_date, created_by_requestor_username, updated_by_date, updated_by_requestor_username)
-VALUES ('COMPANY_NAME_SEED', 'Company Name Seed', '2020-07-27', 'SEED', NULL, NULL);
+VALUES ('TEST_COMPANY_NAME_SEED', 'TEST Company Name Seed', '2020-07-27', 'SEED', NULL, NULL);
 INSERT INTO sotetest.referencetable (reference_name, reference_name_display, created_by_date, created_by_requestor_username, updated_by_date, updated_by_requestor_username)
-VALUES ('PORT_OF_ENTRY_SEED', 'Port of Entry Seed', '2020-07-27', 'SEED', '2020-07-27', 'HARVESTER');
+VALUES ('TEST_PORT_OF_ENTRY_SEED', 'TEST Port of Entry Seed', '2020-07-27', 'SEED', '2020-07-27', 'HARVESTER');
 
 /*
  * Example of a simple parent table for parent/child structure.
  */
 CREATE TABLE sotetest.parenttable
 (
-    parenttable_id                  BIGSERIAL                           NOT NULL
+    parenttable_id                BIGSERIAL                           NOT NULL
         CONSTRAINT parenttable_pkey
             PRIMARY KEY,
-    row_version                     BIGINT  DEFAULT 1                   NOT NULL,
-    user_organizations_id           BIGINT                              NOT NULL,
-    user_is_assigned                BOOLEAN DEFAULT FALSE               NOT NULL,
-    created_by_date                 DATE    DEFAULT ('now'::TEXT)::DATE NOT NULL,
-    created_by_requestor_username   VARCHAR(255)                        NOT NULL,
-    updated_by_date                 DATE,
-    updated_by_requestor_username   VARCHAR(255),
-    user_phone_country_code         VARCHAR(10),
-    user_phone_number               VARCHAR(20),
-    user_email                      VARCHAR(255),
-    user_last_name                  VARCHAR(100),
-    user_first_name                 VARCHAR(50),
-    user_middle_name                VARCHAR(50),
-    user_name_prefix                VARCHAR(5),
-    user_name_suffix                VARCHAR(10),
-    user_hire_date                  DATE,
-    user_driver_license_id          VARCHAR(50),
-    user_driver_license_expiry_date DATE,
-    user_wage                       REAL,
-    user_pay_period                 VARCHAR(15),
-    device_token                    VARCHAR(250),
-    informational_user              BOOLEAN DEFAULT FALSE               NOT NULL,
-    user_is_active                  BOOLEAN DEFAULT FALSE               NOT NULL,
-    client_company_id               BIGINT
+    row_version                   BIGINT  DEFAULT 1                   NOT NULL,
+    parent_table_name             VARCHAR(100)                        NOT NULL,
+    boolean_default_false         BOOLEAN DEFAULT FALSE               NOT NULL,
+    created_by_date               DATE    DEFAULT ('now'::TEXT)::DATE NOT NULL,
+    created_by_requestor_username VARCHAR(255)                        NOT NULL,
+    updated_by_date               DATE,
+    updated_by_requestor_username VARCHAR(255),
+    general_string_nullable       VARCHAR(255),
+    date_no_default               DATE,
+    float_no_default              REAL
 );
 
 COMMENT ON TABLE sotetest.parenttable IS 'This is a test table and is not to be used except for testing.';
@@ -83,9 +69,9 @@ COMMENT ON COLUMN sotetest.parenttable.parenttable_id IS 'This is a system gener
 
 COMMENT ON COLUMN sotetest.parenttable.row_version IS 'This is a system generated number that is used to control updates to the record';
 
-COMMENT ON COLUMN sotetest.parenttable.user_organizations_id IS 'This is the organizations the user belongs to and has access too';
+COMMENT ON COLUMN sotetest.parenttable.parent_table_name IS 'Test name used for the parent table row';
 
-COMMENT ON COLUMN sotetest.parenttable.user_is_assigned IS 'User is assigned a task in the system';
+COMMENT ON COLUMN sotetest.parenttable.boolean_default_false IS 'Tests a boolean column with default false';
 
 COMMENT ON COLUMN sotetest.parenttable.created_by_date IS 'When the user entry was entered in the system';
 
@@ -95,66 +81,49 @@ COMMENT ON COLUMN sotetest.parenttable.updated_by_date IS 'When the user entry w
 
 COMMENT ON COLUMN sotetest.parenttable.updated_by_requestor_username IS 'The Cognito username for the person who is requesting the users update';
 
-COMMENT ON COLUMN sotetest.parenttable.user_phone_country_code IS 'User phones country code';
+COMMENT ON COLUMN sotetest.parenttable.general_string_nullable IS 'General string column that is nullable';
 
-COMMENT ON COLUMN sotetest.parenttable.user_phone_number IS 'User phones number';
+COMMENT ON COLUMN sotetest.parenttable.date_no_default IS 'Date column that is nullable';
 
-COMMENT ON COLUMN sotetest.parenttable.user_email IS 'Users email';
-
-COMMENT ON COLUMN sotetest.parenttable.user_last_name IS 'This is the last name of the user';
-
-COMMENT ON COLUMN sotetest.parenttable.user_first_name IS 'This is the first name of the user';
-
-COMMENT ON COLUMN sotetest.parenttable.user_middle_name IS 'This is the middle name of the user';
-
-COMMENT ON COLUMN sotetest.parenttable.user_name_prefix IS 'The title the user is addressed by';
-
-COMMENT ON COLUMN sotetest.parenttable.user_name_suffix IS 'This is the position, educational degree, accreditation, office, honor of the user';
-
-COMMENT ON COLUMN sotetest.parenttable.user_hire_date IS 'When the user was hired by the organization';
-
-COMMENT ON COLUMN sotetest.parenttable.user_driver_license_id IS 'The identifier on the users license';
-
-COMMENT ON COLUMN sotetest.parenttable.user_driver_license_expiry_date IS 'The date the license expires';
-
-COMMENT ON COLUMN sotetest.parenttable.user_wage IS 'The amount the person is paid for the provided pay period';
-
-COMMENT ON COLUMN sotetest.parenttable.user_pay_period IS 'The frequency the person is paid';
-
-COMMENT ON COLUMN sotetest.parenttable.device_token IS 'The value Google provides to identify a phone';
-
-COMMENT ON COLUMN sotetest.parenttable.informational_user IS 'identifies system user(TRUE) and informational user(FALSE)';
-
-COMMENT ON COLUMN sotetest.parenttable.user_is_active IS 'Activates and deactivates user';
-
-COMMENT ON COLUMN sotetest.parenttable.client_company_id IS 'The system id that is used to control access to organization customer information';
+COMMENT ON COLUMN sotetest.parenttable.float_no_default IS 'Date column that is nullable';
 
 ALTER TABLE sotetest.parenttable
     OWNER TO sote;
 
-CREATE UNIQUE INDEX parenttable_orgid_phone_uindex
-    ON sotetest.parenttable (user_phone_number, user_phone_country_code);
-
-CREATE UNIQUE INDEX parenttable_orgid_email_uindex
-    ON sotetest.parenttable (user_email);
+CREATE UNIQUE INDEX parenttable_general_string_uindex
+    ON sotetest.parenttable (parent_table_name);
 
 CREATE INDEX parenttable_orgid_not_uindex
-    ON sotetest.parenttable (user_organizations_id);
+    ON sotetest.parenttable (float_no_default);
 
-CREATE INDEX parenttable_clientcompanyid_not_uindex
-    ON sotetest.parenttable (client_company_id);
 
 CREATE INDEX parenttable_orgid_clientcompanyid_not_uindex
-    ON sotetest.parenttable (client_company_id, user_organizations_id);
+    ON sotetest.parenttable (boolean_default_false, float_no_default);
 
+INSERT INTO sotetest.parenttable (parenttable_id, row_version, parent_table_name, boolean_default_false, created_by_date, created_by_requestor_username, updated_by_date, updated_by_requestor_username,
+                                  general_string_nullable, date_no_default, float_no_default)
+VALUES (12453, 1, 'name 1', FALSE, '2020-08-25', 'tyfg4581-jiuh2-3325jghrfd', '2020-08-25', 'tyfg4581-jiuh2-3325jghrfd', 'test field 1', NULL, 12.26);
+INSERT INTO sotetest.parenttable (parenttable_id, row_version, parent_table_name, boolean_default_false, created_by_date, created_by_requestor_username, updated_by_date, updated_by_requestor_username,
+                                  general_string_nullable, date_no_default, float_no_default)
+VALUES (2, 1, 'name 2', FALSE, '2020-08-25', 'seed', NULL, NULL, 'test field 2', NULL, NULL);
+INSERT INTO sotetest.parenttable (parenttable_id, row_version, parent_table_name, boolean_default_false, created_by_date, created_by_requestor_username, updated_by_date, updated_by_requestor_username,
+                                  general_string_nullable, date_no_default, float_no_default)
+VALUES (54201, 1, 'name 3', FALSE, '2020-08-25', 'tyfg4581-jiuh2-3325jghrfd', NULL, NULL, 'test field 5', '2020-08-04', NULL);
+INSERT INTO sotetest.parenttable (parenttable_id, row_version, parent_table_name, boolean_default_false, created_by_date, created_by_requestor_username, updated_by_date, updated_by_requestor_username,
+                                  general_string_nullable, date_no_default, float_no_default)
+VALUES (3, 1, 'name 4', TRUE, '2020-08-25', 'seed', '2020-08-13', 'tyfg4581-jiuh2-3325jghrfd', 'test fld 7', '2020-06-01', 1253.241);
 
 /*
  * Example of a simple child table for parent/child structure.
  */
 CREATE TABLE sotetest.parentchildtable
 (
-    parenttable_id   BIGINT NOT NULL,
-    cognito_username VARCHAR(255)
+    childtable_id    BIGSERIAL NOT NULL
+        CONSTRAINT childtable_pkey
+            PRIMARY KEY,
+    parenttable_id   BIGINT    NOT NULL,
+    cognito_username VARCHAR(255),
+    reference_name   VARCHAR(25)
 );
 
 COMMENT ON TABLE sotetest.parentchildtable IS 'This is a test table and is not to be used except for testing.';
@@ -163,9 +132,34 @@ COMMENT ON COLUMN sotetest.parentchildtable.parenttable_id IS 'This is a system 
 
 COMMENT ON COLUMN sotetest.parentchildtable.cognito_username IS 'The Cognito username for this user';
 
+COMMENT ON COLUMN sotetest.parentchildtable.reference_name IS 'This is the name of the reference type used by Sote System';
+
 ALTER TABLE sotetest.parentchildtable
     OWNER TO sote;
 
+CREATE UNIQUE INDEX childtable_parenttable_uindex
+    ON sotetest.parenttable (parenttable_id);
+
+INSERT INTO sotetest.parentchildtable (childtable_id, parenttable_id, cognito_username, reference_name)
+VALUES (1, 12345, 'Scott', 'TEST_PORT_OF_ENTRY');
+INSERT INTO sotetest.parentchildtable (childtable_id, parenttable_id, cognito_username, reference_name)
+VALUES (2, 12345, 'April', NULL);
+INSERT INTO sotetest.parentchildtable (childtable_id, parenttable_id, cognito_username, reference_name)
+VALUES (5, 12345, 'Charity', NULL);
+INSERT INTO sotetest.parentchildtable (childtable_id, parenttable_id, cognito_username, reference_name)
+VALUES (3, 12345, 'Pam', 'TEST_COMPANY_NAME_SEED');
+INSERT INTO sotetest.parentchildtable (childtable_id, parenttable_id, cognito_username, reference_name)
+VALUES (4, 12345, 'Steve', 'TEST_COMPANY_NAME_SEED');
+INSERT INTO sotetest.parentchildtable (childtable_id, parenttable_id, cognito_username, reference_name)
+VALUES (6, 2, NULL, NULL);
+INSERT INTO sotetest.parentchildtable (childtable_id, parenttable_id, cognito_username, reference_name)
+VALUES (7, 2, NULL, NULL);
+INSERT INTO sotetest.parentchildtable (childtable_id, parenttable_id, cognito_username, reference_name)
+VALUES (8, 2, NULL, NULL);
+INSERT INTO sotetest.parentchildtable (childtable_id, parenttable_id, cognito_username, reference_name)
+VALUES (9, 2, NULL, NULL);
+INSERT INTO sotetest.parentchildtable (childtable_id, parenttable_id, cognito_username, reference_name)
+VALUES (10, 2, NULL, NULL);
 
 /*
  * Example of a many to many structure.
@@ -193,6 +187,21 @@ COMMENT ON COLUMN sotetest.parentsideonetable.created_by_requestor_username IS '
 ALTER TABLE sotetest.parentsideonetable
     OWNER TO sote;
 
+INSERT INTO sotetest.parentsideonetable (parentsideonetable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (100, 1, '2020-08-25', 'asdf-wert-sdfg-465');
+INSERT INTO sotetest.parentsideonetable (parentsideonetable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (200, 1, '2020-08-25', 'seed');
+INSERT INTO sotetest.parentsideonetable (parentsideonetable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (300, 1, '2020-08-25', 'asdf-qwerdfgh-dfghdfgh');
+INSERT INTO sotetest.parentsideonetable (parentsideonetable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (400, 1, '2020-08-25', 'asdf-qwerdfgh-dfghdfgh');
+INSERT INTO sotetest.parentsideonetable (parentsideonetable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (500, 1, '2020-08-25', 'asdf-qwerdfgh-dfghdfgh');
+INSERT INTO sotetest.parentsideonetable (parentsideonetable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (600, 1, '2020-08-25', 'asdf-wert-sdfg-465');
+INSERT INTO sotetest.parentsideonetable (parentsideonetable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (700, 1, '2020-08-25', 'asdf-wert-sdfg-465');
+
 CREATE TABLE sotetest.parentsidetwotable
 (
     parentsidetwotable_id         BIGSERIAL                          NOT NULL
@@ -215,6 +224,21 @@ COMMENT ON COLUMN sotetest.parentsidetwotable.created_by_requestor_username IS '
 
 ALTER TABLE sotetest.parentsidetwotable
     OWNER TO sote;
+
+INSERT INTO sotetest.parentsidetwotable (parentsidetwotable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (100100, 1, '2020-08-25', 'asdf-wert-sdfg-465');
+INSERT INTO sotetest.parentsidetwotable (parentsidetwotable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (100200, 1, '2020-08-25', 'seed');
+INSERT INTO sotetest.parentsidetwotable (parentsidetwotable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (100300, 1, '2020-08-25', 'asdf-qwerdfgh-dfghdfgh');
+INSERT INTO sotetest.parentsidetwotable (parentsidetwotable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (100400, 1, '2020-08-25', 'asdf-qwerdfgh-dfghdfgh');
+INSERT INTO sotetest.parentsidetwotable (parentsidetwotable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (100500, 1, '2020-08-25', 'asdf-qwerdfgh-dfghdfgh');
+INSERT INTO sotetest.parentsidetwotable (parentsidetwotable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (100600, 1, '2020-08-25', 'asdf-wert-sdfg-465');
+INSERT INTO sotetest.parentsidetwotable (parentsidetwotable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (100700, 1, '2020-08-25', 'asdf-wert-sdfg-465');
 
 CREATE TABLE sotetest.manytomanytable
 (
@@ -247,3 +271,16 @@ ALTER TABLE sotetest.manytomanytable
 
 CREATE UNIQUE INDEX vehicles_jobcards_uindex
     ON sotetest.manytomanytable (parentsideonetable_id, parentsidetwotable_id);
+
+INSERT INTO sotetest.manytomanytable (manytomanytable_id, parentsideonetable_id, parentsidetwotable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (1, 100, 100300, 1, '2020-08-25', 'seed');
+INSERT INTO sotetest.manytomanytable (manytomanytable_id, parentsideonetable_id, parentsidetwotable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (2, 300, 100300, 1, '2020-08-25', 'asdf-werty-dfgh-655465');
+INSERT INTO sotetest.manytomanytable (manytomanytable_id, parentsideonetable_id, parentsidetwotable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (3, 200, 100100, 1, '2020-08-25', 'asdf-werty-dfgh-655465');
+INSERT INTO sotetest.manytomanytable (manytomanytable_id, parentsideonetable_id, parentsidetwotable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (4, 200, 100200, 1, '2020-08-25', 'asdf-werty-dfgh-655465');
+INSERT INTO sotetest.manytomanytable (manytomanytable_id, parentsideonetable_id, parentsidetwotable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (5, 400, 100600, 1, '2020-08-25', 'seed');
+INSERT INTO sotetest.manytomanytable (manytomanytable_id, parentsideonetable_id, parentsidetwotable_id, row_version, created_by_date, created_by_requestor_username)
+VALUES (6, 100, 100100, 1, '2020-08-25', 'asdf-werty-dfgh-655465');
