ed55ab1a7917b5c8bba1e1f7155fb327e031a23b
stephen morris kinyori <kinyorimorris@gmail.com>
2020-09-29 15:06:51 -0700

    Multiple Changes
    Centralized all testing constants
    Add TEST to the beginning of test constants
    Removed Consumer test

--- sMessage/smessage_test.go
+++ sMessage/smessage_test.go
@@ -9,22 +9,23 @@ import (
 )
 
 const (
-	STREAMNAME          = "TEST_STREAM_NAME"
-	STREAMSUBJECT       = "TEST_STREAM_NAME"
-	STREAMCREDITIALFILE = "./test_fake.creds"
-	STREAMFAKETOKEN     = "eyJ0eXAiFAKEd3QiLCJhbGciOiJlZDI1NTE5In0.eyJqdGkiOiIyCREDSkQzWjZUUUxFQ1FKUkNSNEEzS1M2SzZRM1hOWDcyTTJLRUFUQjRJFAKEUVJFVVpRIiwiaWF0IjoxNTk4OTE1Njc5LCJpc3MiOiJBQVZOVzU0UFJCREDSEUzWEM2SEVIQUNTTEM2RkRQWFIyV1g2M1NGTjJVS0k1R0VLUE9WMktPTiIsIm5hbWUiOiJiZSIsInN1YiI6IlVER0VZWUVJWUFMTzdTUFpPWlFPRDVDUUlLWVZXRlNMNFVQRUFTNDc0NkpTRkFSMk5QS0hUSEdPIiwidHlwZSI6InVzZXIiLCJuYXRzIjp7InB1YiI6e30sInN1YiI6e319fQ.qKhcZTO8P1ZxM28B5fZ-5NOrKB3GUv_60jVMohzR2p1PqJR5rmXLA22IpOYnApJXmkI8z1UJNJ7CHj6wpottCg\n"
-	CONSUMERDURABLENAME = "TEST_CONSUMER_NAME"
-	SUBJECTFILETER      = "TEST_STREAM_NAME.TEST1"
-	NONEXISTINGFILE     = "./file_does_not.exist"
-	MAXRECONNECT        = 5
-	RECONNECTWAIT       = 1 * time.Second
-	SYNADIAURL          = "euwest1.aws.ngs.global"
-	TIMEOUT             = 1 * time.Second
-	BADURL              = "google.com"
+	TESTSTREAMNAME          = "TEST_STREAM_NAME"
+	TESTSTREAMSUBJECT       = "TEST_STREAM_NAME"
+	TESTSTREAMCREDITIALFILE = "./test_fake.creds"
+	TESTSTREAMFAKETOKEN     = "eyJ0eXAiFAKEd3QiLCJhbGciOiJlZDI1NTE5In0.eyJqdGkiOiIyCREDSkQzWjZUUUxFQ1FKUkNSNEEzS1M2SzZRM1hOWDcyTTJLRUFUQjRJFAKEUVJFVVpRIiwiaWF0IjoxNTk4OTE1Njc5LCJpc3MiOiJBQVZOVzU0UFJCREDSEUzWEM2SEVIQUNTTEM2RkRQWFIyV1g2M1NGTjJVS0k1R0VLUE9WMktPTiIsIm5hbWUiOiJiZSIsInN1YiI6IlVER0VZWUVJWUFMTzdTUFpPWlFPRDVDUUlLWVZXRlNMNFVQRUFTNDc0NkpTRkFSMk5QS0hUSEdPIiwidHlwZSI6InVzZXIiLCJuYXRzIjp7InB1YiI6e30sInN1YiI6e319fQ.qKhcZTO8P1ZxM28B5fZ-5NOrKB3GUv_60jVMohzR2p1PqJR5rmXLA22IpOYnApJXmkI8z1UJNJ7CHj6wpottCg\n"
+	TESTDURABLENAME         = "TEST_DURABLE_NAME"
+	TESTDELIVERSUBJECT      = "TEST_STREAM_NAME.TEST1"
+	TESTSUBJECTFILETER      = "TEST_STREAM_NAME.TEST1"
+	TESTNONEXISTINGFILE     = "./file_does_not.exist"
+	TESTMAXRECONNECT        = 5
+	TESTRECONNECTWAIT       = 1 * time.Second
+	TESTSYNADIAURL          = "euwest1.aws.ngs.global"
+	TESTTIMEOUT             = 1 * time.Second
+	TESTBADURL              = "google.com"
 )
 
 func TestSetStreamName(t *testing.T) {
-	if _, soteErr := SetStreamName(STREAMNAME); soteErr.ErrCode != nil {
+	if _, soteErr := SetStreamName(TESTSTREAMNAME); soteErr.ErrCode != nil {
 		t.Errorf("SetStreamName Failed: Expected error code to be nil")
 	}
 
@@ -33,7 +34,7 @@ func TestSetStreamName(t *testing.T) {
 	}
 }
 func TestSetCredentials(t *testing.T) {
-	if _, soteErr := SetCredentialsFile(STREAMCREDITIALFILE); soteErr.ErrCode != nil {
+	if _, soteErr := SetCredentialsFile(TESTSTREAMCREDITIALFILE); soteErr.ErrCode != nil {
 		t.Errorf("SetCredentialsFile Failed: Expected error code to be nil")
 	}
 
@@ -41,11 +42,11 @@ func TestSetCredentials(t *testing.T) {
 		t.Errorf("SetCredentialsFile Failed: Expected error code of 200513")
 	}
 
-	if _, soteErr := SetCredentialsFile(NONEXISTINGFILE); soteErr.ErrCode != 600010 {
+	if _, soteErr := SetCredentialsFile(TESTNONEXISTINGFILE); soteErr.ErrCode != 600010 {
 		t.Errorf("SetCredentialsFile Failed: Expected error code of 600010")
 	}
 
-	if _, soteErr := SetCredentialsToken(STREAMFAKETOKEN); soteErr.ErrCode != nil {
+	if _, soteErr := SetCredentialsToken(TESTSTREAMFAKETOKEN); soteErr.ErrCode != nil {
 		t.Errorf("SetCredentialsToken Failed: Expected error code to be nil")
 	}
 
@@ -54,7 +55,7 @@ func TestSetCredentials(t *testing.T) {
 	}
 }
 func TestSetReconnectOptions(t *testing.T) {
-	if _, soteErr := SetReconnectOptions(MAXRECONNECT, RECONNECTWAIT); soteErr.ErrCode != nil {
+	if _, soteErr := SetReconnectOptions(TESTMAXRECONNECT, TESTRECONNECTWAIT); soteErr.ErrCode != nil {
 		t.Errorf("SetReconnectOptions Failed: Expected error code to be nil")
 	}
 
@@ -62,16 +63,16 @@ func TestSetReconnectOptions(t *testing.T) {
 		t.Errorf("SetReconnectOptions Failed: Expected error code of 200512")
 	}
 
-	if _, soteErr := SetReconnectOptions(0, RECONNECTWAIT); soteErr.ErrCode != 200513 {
+	if _, soteErr := SetReconnectOptions(0, TESTRECONNECTWAIT); soteErr.ErrCode != 200513 {
 		t.Errorf("SetReconnectOptions Failed: Expected error code of 200513")
 	}
 
-	if _, soteErr := SetReconnectOptions(MAXRECONNECT, 0); soteErr.ErrCode != 200513 {
+	if _, soteErr := SetReconnectOptions(TESTMAXRECONNECT, 0); soteErr.ErrCode != 200513 {
 		t.Errorf("SetReconnectOptions Failed: Expected error code of 200513")
 	}
 }
 func TestSetTimeOut(t *testing.T) {
-	if _, soteErr := SetTimeOut(TIMEOUT); soteErr.ErrCode != nil {
+	if _, soteErr := SetTimeOut(TESTTIMEOUT); soteErr.ErrCode != nil {
 		t.Errorf("SetTimeOut Failed: Expected error code to be nil")
 	}
 
@@ -80,31 +81,31 @@ func TestSetTimeOut(t *testing.T) {
 	}
 }
 func TestSetAllOptions(t *testing.T) {
-	if _, soteErr := SetAllOptions(STREAMNAME, STREAMCREDITIALFILE, STREAMFAKETOKEN, MAXRECONNECT, RECONNECTWAIT, TIMEOUT); soteErr.ErrCode != nil {
+	if _, soteErr := SetAllOptions(TESTSTREAMNAME, TESTSTREAMCREDITIALFILE, TESTSTREAMFAKETOKEN, TESTMAXRECONNECT, TESTRECONNECTWAIT, TESTTIMEOUT); soteErr.ErrCode != nil {
 		t.Errorf("SetAllOptions Failed: Expected error code to be nil")
 	}
 
-	if _, soteErr := SetAllOptions("", STREAMCREDITIALFILE, STREAMFAKETOKEN, MAXRECONNECT, RECONNECTWAIT, TIMEOUT); soteErr.ErrCode != 200513 {
+	if _, soteErr := SetAllOptions("", TESTSTREAMCREDITIALFILE, TESTSTREAMFAKETOKEN, TESTMAXRECONNECT, TESTRECONNECTWAIT, TESTTIMEOUT); soteErr.ErrCode != 200513 {
 		t.Errorf("SetAllOptions Failed: Expected error code of 200513")
 	}
 
-	if _, soteErr := SetAllOptions(STREAMNAME, "", STREAMFAKETOKEN, MAXRECONNECT, RECONNECTWAIT, TIMEOUT); soteErr.ErrCode != 200513 {
+	if _, soteErr := SetAllOptions(TESTSTREAMNAME, "", TESTSTREAMFAKETOKEN, TESTMAXRECONNECT, TESTRECONNECTWAIT, TESTTIMEOUT); soteErr.ErrCode != 200513 {
 		t.Errorf("SetAllOptions Failed: Expected error code of 200513")
 	}
 
-	if _, soteErr := SetAllOptions(STREAMNAME, STREAMCREDITIALFILE, "", MAXRECONNECT, RECONNECTWAIT, TIMEOUT); soteErr.ErrCode != 200513 {
+	if _, soteErr := SetAllOptions(TESTSTREAMNAME, TESTSTREAMCREDITIALFILE, "", TESTMAXRECONNECT, TESTRECONNECTWAIT, TESTTIMEOUT); soteErr.ErrCode != 200513 {
 		t.Errorf("SetAllOptions Failed: Expected error code of 200513")
 	}
 
-	if _, soteErr := SetAllOptions(STREAMNAME, STREAMCREDITIALFILE, STREAMFAKETOKEN, 0, RECONNECTWAIT, TIMEOUT); soteErr.ErrCode != 200513 {
+	if _, soteErr := SetAllOptions(TESTSTREAMNAME, TESTSTREAMCREDITIALFILE, TESTSTREAMFAKETOKEN, 0, TESTRECONNECTWAIT, TESTTIMEOUT); soteErr.ErrCode != 200513 {
 		t.Errorf("SetAllOptions Failed: Expected error code of 200513")
 	}
 
-	if _, soteErr := SetAllOptions(STREAMNAME, STREAMCREDITIALFILE, STREAMFAKETOKEN, MAXRECONNECT, 0, TIMEOUT); soteErr.ErrCode != 200513 {
+	if _, soteErr := SetAllOptions(TESTSTREAMNAME, TESTSTREAMCREDITIALFILE, TESTSTREAMFAKETOKEN, TESTMAXRECONNECT, 0, TESTTIMEOUT); soteErr.ErrCode != 200513 {
 		t.Errorf("SetAllOptions Failed: Expected error code of 200513")
 	}
 
-	if _, soteErr := SetAllOptions(STREAMNAME, STREAMCREDITIALFILE, STREAMFAKETOKEN, MAXRECONNECT, RECONNECTWAIT, 0); soteErr.ErrCode != 200513 {
+	if _, soteErr := SetAllOptions(TESTSTREAMNAME, TESTSTREAMCREDITIALFILE, TESTSTREAMFAKETOKEN, TESTMAXRECONNECT, TESTRECONNECTWAIT, 0); soteErr.ErrCode != 200513 {
 		t.Errorf("SetAllOptions Failed: Expected error code of 200513")
 	}
 }
@@ -117,33 +118,19 @@ func TestConnect(t *testing.T) {
 	if opts, soteErr = SetCredentialsFile("/Users/syacko/.nkeys/creds/synadia/NATS_CONNECT/NATS_CONNECT.creds"); soteErr.ErrCode != nil {
 		t.Errorf("SetCredentialsFile Failed: Expected error code to be nil")
 	} else {
-		if _, soteErr = Connect(SYNADIAURL, opts); soteErr.ErrCode != nil {
+		if _, soteErr = Connect(TESTSYNADIAURL, opts); soteErr.ErrCode != nil {
 			t.Errorf("Connect Failed: Expected error code to be nil")
 		}
 	}
 
-	if _, soteErr = Connect(SYNADIAURL, nil); soteErr.ErrCode != 200513 {
+	if _, soteErr = Connect(TESTSYNADIAURL, nil); soteErr.ErrCode != 200513 {
 		t.Errorf("Connect Failed: Expected error code of 200513")
 	}
 
-	if _, soteErr = Connect(BADURL, opts); soteErr.ErrCode != 603999 {
+	if _, soteErr = Connect(TESTBADURL, opts); soteErr.ErrCode != 603999 {
 		t.Errorf("Connect Failed: Expected error code of 603999")
 	}
+	if _, soteErr = Connect("", opts); soteErr.ErrCode != 200513 {
+		t.Errorf("Connect Failed: Expected error code of 200513")
+	}
 }
-// func TestCreateConsumer(t *testing.T) {
-// 	var (
-// 		opts    []nats.Option
-// 		soteErr sError.SoteError
-// 		nc      *nats.Conn
-// 	)
-//
-// 	if opts, soteErr = SetCredentialsFile("/Users/syacko/.nkeys/creds/synadia/NATS_CONNECT/NATS_CONNECT.creds"); soteErr.ErrCode != nil {
-// 		t.Errorf("SetCredentialsFile Failed: Expected error code to be nil")
-// 	} else {
-// 		if nc, soteErr = Connect(SYNADIAURL, opts); soteErr.ErrCode != nil {
-// 			t.Errorf("Connect Failed: Expected error code to be nil")
-// 		}
-// 	}
-//
-// 	_, soteErr = CreateConsumer(STREAMNAME, CONSUMERDURABLENAME, SUBJECTFILETER, nc)
-// }
