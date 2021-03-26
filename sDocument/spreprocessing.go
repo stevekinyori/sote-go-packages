2d236c24b2b38657d940b037785a0acb65ad1ee1
stephen morris kinyori <kinyorimorris@gmail.com>
2021-03-26 09:06:14 +0300

    Create method to convert image to grayscale

--- sDocument/spreprocessing.go
+++ sDocument/spreprocessing.go
@@ -2,47 +2,30 @@
 package sDocument
 
 import (
+	"os"
+
 	"gitlab.com/soteapps/packages/v2021/sError"
 	"gitlab.com/soteapps/packages/v2021/sLogger"
 	"gocv.io/x/gocv"
-	"os"
 )
 
 type PreprocessManager struct {
+	sOriginalImage gocv.Mat
 }
 
-func NewPreprocessor() (preprocessManagerPtr *PreprocessManager, soteErr sError.SoteError) {
+func NewPreprocessor(sFilePath string) (preprocessManagerPtr *PreprocessManager, soteErr sError.SoteError) {
 	sLogger.DebugMethod()
 
-	preprocessManagerPtr = &PreprocessManager{}
-
-	return
-}
-
-/* CorrectSkew fixes direction and angle of skewed images */
-func (pm *PreprocessManager) CorrectSkew(sFilePath string) (sGrayScaleImage gocv.Mat, soteErr sError.SoteError) {
-	sLogger.DebugMethod()
-
-	var tGrayScaleImage gocv.Mat = gocv.NewMat()
-
-	if _, soteErr = pm.checkIfPathExists(sFilePath); soteErr.ErrCode == nil {
-		sOriginalImage := gocv.IMRead(sFilePath, -1) // Load image and return it in original format
-		sGrayScaleImage = gocv.NewMat() // Create a new empty mat
-		//convert the image to grayscale
-		gocv.CvtColor(sOriginalImage, &tGrayScaleImage, gocv.ColorBGRToGray)
-		//Flip the foreground and background to ensure foreground is now "white" and background is "black"
-		gocv.BitwiseNot(tGrayScaleImage, &sGrayScaleImage)
-
-		window := gocv.NewWindow("Hello")
-		window.IMShow(sGrayScaleImage)
-		window.WaitKey(0)
+	if _, soteErr = CheckIfPathExists(sFilePath); soteErr.ErrCode == nil {
+		tOriginalImage := gocv.IMRead(sFilePath, -1) // Load image and return it in original format
+		preprocessManagerPtr = &PreprocessManager{sOriginalImage: tOriginalImage}
 	}
 
 	return
 }
 
-/* checkIfPathExists determines whether path to uploaded file exists */
-func (pm *PreprocessManager) checkIfPathExists(sFilePath string) (sExistingFilePath os.FileInfo, soteErr sError.SoteError) {
+/* CheckIfPathExists determines whether path to uploaded file exists */
+func CheckIfPathExists(sFilePath string) (sExistingFilePath os.FileInfo, soteErr sError.SoteError) {
 	sLogger.DebugMethod()
 
 	var sErr error
@@ -51,8 +34,44 @@ func (pm *PreprocessManager) checkIfPathExists(sFilePath string) (sExistingFileP
 		//TODO Add sError support for incorrect file path
 		soteErr = sError.GetSError(210400, nil, sError.EmptyMap)
 		sLogger.Info(soteErr.FmtErrMsg)
+		//TODO Determine whether program should panic if upload path is invalid
 		//panic("sDocument.checkIfPathExists failed")
 	}
+
+	return
+}
+
+/* CorrectSkew fixes direction and angle of skewed images */
+func (pm *PreprocessManager) CorrectSkew() (sGrayScaleImage gocv.Mat, soteErr sError.SoteError) {
+	sLogger.DebugMethod()
+
+	var (
+	//sBitwiseImage   = gocv.NewMat()
+	//tThresholdedImage = gocv.NewMat()
+	//sThreshold        float32
+	)
+
+	sGrayScaleImage = pm.convertImageToGrayScale()
+	////Flip the foreground and background to ensure foreground is now "white" and background is "black"
+	//gocv.BitwiseNot(tGrayScaleImage, &sGrayScaleImage)
+	//// Threshold image, set all foreground pixels to 255 and all background pixels to 0
+	//sThreshold = gocv.Threshold(sGrayScaleImage, &tThresholdedImage, 0, 255, gocv.ThresholdBinary|gocv.ThresholdOtsu)
+	//fmt.Println(sThreshold)
+
+	window := gocv.NewWindow("Hello")
+	window.IMShow(sGrayScaleImage)
+	window.WaitKey(0)
+
+	return
+}
+
+/* convertImageToGrayScale Converts image to grayscale */
+func (pm PreprocessManager) convertImageToGrayScale() (sGrayScaleImage gocv.Mat) {
+	sLogger.DebugMethod()
+
+	sGrayScaleImage = gocv.NewMat()
+
+	gocv.CvtColor(pm.sOriginalImage, &sGrayScaleImage, gocv.ColorBGRToGray)
 
 	return
 }
--- sDocument/spreprocessing_test.go
+++ sDocument/spreprocessing_test.go
@@ -5,24 +5,27 @@ import (
 )
 
 func TestNewPreprocessor(t *testing.T) {
-	if _, soteErr := NewPreprocessor(); soteErr.ErrCode != nil {
+	if _, soteErr := NewPreprocessor("../img/testing_materials/Container Guarantee Form back.jpg"); soteErr.ErrCode != nil {
 		t.Errorf("NewPreprocessor failed: Expected soteErr to be nil: %v", soteErr.FmtErrMsg)
+	}
+
+	if _, soteErr := NewPreprocessor("img/testing_materials/Container Guarantee Form back.jpg"); soteErr.ErrCode != 210400 {
+		t.Errorf("NewPreprocessor failed: Expected error code of 210400: %v", soteErr.FmtErrMsg)
+	}
+}
+func TestCheckIfPathExists(t *testing.T) {
+	if _, soteErr := CheckIfPathExists("../img/testing_materials/Container Guarantee Form back.jpg"); soteErr.ErrCode != nil {
+		t.Errorf("CheckIfFilePathExists failed: Expected soteErr to be nil:%v", soteErr.FmtErrMsg)
+	}
+
+	if _, soteErr := CheckIfPathExists("img/testing_materials/Container Guarantee Form back.jpg"); soteErr.ErrCode != 210400 {
+		t.Errorf("CheckIfFilePathExists failed: Expected error code of 210400: %v", soteErr.FmtErrMsg)
 	}
 }
 func TestPreprocessManager_CorrectSkew(t *testing.T) {
-	if pm, soteErr := NewPreprocessor(); soteErr.ErrCode == nil {
-		if _, soteErr = pm.CorrectSkew("../img/testing_materials/Container Guarantee Form back.jpg"); soteErr.ErrCode != nil {
+	if pm, soteErr := NewPreprocessor("../img/testing_materials/Container Guarantee Form back.jpg"); soteErr.ErrCode == nil {
+		if _, soteErr = pm.CorrectSkew(); soteErr.ErrCode != nil {
 			t.Errorf("CorrectSkew failed: Expected soteErr to be nil: %v", soteErr.FmtErrMsg)
 		}
 	}
-
-	if pm, soteErr := NewPreprocessor(); soteErr.ErrCode == nil {
-		if _, soteErr = pm.CorrectSkew("img/testing_materials/Container Guarantee Form back.jpg"); soteErr.ErrCode != 210400 {
-			t.Errorf("CorrectSkew failed: Expected error code of 210400: %v", soteErr.FmtErrMsg)
-		}
-	}
 }
-
-
-
-
