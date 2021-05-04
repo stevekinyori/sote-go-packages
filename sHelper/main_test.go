b4db3508192abe04cc4ab074f176536839e259d7
stephen morris kinyori <kinyorimorris@gmail.com>
2021-05-04 13:27:34 -0700

    created mail server

--- sHelper/main_test.go
+++ sHelper/main_test.go
@@ -3,8 +3,11 @@ package sHelper
 import "testing"
 
 func TestMail(t *testing.T) {
-	mail := NewMail("staging", "Test", "David Gofman")
-	mail.To("dgofman@gmail.com <Personal Email>")
-	//mail.Cs("david_gofman@getsote.com <Company Email>")
-	mail.Send("Hello World1", "<b>Hello World2<b>")
+	mail := NewMail("staging", "Test Subject", "David Gofman")
+	mail.To("dgofman@gmail.com").Name("Personal Email")
+	mail.Bcc("david.gofman@getsote.com").Name("Company Email")
+	soteErr := mail.Attachment("./schema_test.json")
+	AssertEqual(t, soteErr.FmtErrMsg, "")
+	//soteErr = mail.Send("Hello World1", "<h1>Hello World2<h1>")
+	AssertEqual(t, soteErr.FmtErrMsg, "")
 }
