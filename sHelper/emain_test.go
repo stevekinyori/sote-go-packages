931bec47542baec1a180664145c5a39cfdb9a2f7
stephen morris kinyori <kinyorimorris@gmail.com>
2021-05-29 10:14:26 -0700

    Update test for Jenkins

--- sHelper/emain_test.go
+++ sHelper/emain_test.go
@@ -11,22 +11,33 @@ import (
 	"gitlab.com/soteapps/packages/v2021/sError"
 )
 
+func newTestEmail(environment, subject string, from ...string) *Email {
+	email := NewEmail(environment, subject, from...)
+	email.GetSmtpUsername = func(application, environment string) (string, sError.SoteError) {
+		return "USERNAME", sError.SoteError{}
+	}
+	email.GetSmtpPassword = func(application, environment string) (string, sError.SoteError) {
+		return "PASSWORD", sError.SoteError{}
+	}
+	return email
+}
+
 func TestEMailSubject(t *testing.T) {
-	email := NewEmail("staging", "Test Subject")
+	email := newTestEmail("staging", "Test Subject")
 	AssertEqual(t, email.environment, "staging")
 	AssertEqual(t, email.subject, "Test Subject")
 	AssertEqual(t, email.from.String(), DEFAULTEMAIL)
 }
 
 func TestEMailSubjectFrom(t *testing.T) {
-	email := NewEmail("staging", "Test Subject", "David Gofman")
+	email := newTestEmail("staging", "Test Subject", "David Gofman")
 	AssertEqual(t, email.environment, "staging")
 	AssertEqual(t, email.subject, "Test Subject")
 	AssertEqual(t, email.from.String(), fmt.Sprintf("%s <%s>", "David Gofman", DEFAULTEMAIL))
 }
 
 func TestEmailTo(t *testing.T) {
-	email := NewEmail("staging", "Test Subject")
+	email := newTestEmail("staging", "Test Subject")
 
 	email.To("admin@getsote.com")
 	email.rcpt = []string{}
@@ -48,7 +59,7 @@ func TestEmailTo(t *testing.T) {
 }
 
 func TestEmailCc(t *testing.T) {
-	email := NewEmail("staging", "Test Subject")
+	email := newTestEmail("staging", "Test Subject")
 
 	email.Cc("admin@getsote.com")
 	email.rcpt = []string{}
@@ -66,7 +77,7 @@ func TestEmailCc(t *testing.T) {
 }
 
 func TestEmailBcc(t *testing.T) {
-	email := NewEmail("staging", "Test Subject")
+	email := newTestEmail("staging", "Test Subject")
 
 	email.Bcc("admin@getsote.com")
 	email.rcpt = []string{}
@@ -84,7 +95,7 @@ func TestEmailBcc(t *testing.T) {
 }
 
 func TestEmailAttachment(t *testing.T) {
-	email := NewEmail("staging", "Test Subject")
+	email := newTestEmail("staging", "Test Subject")
 	soteErr := email.Attachment("foo.txt")
 	// Multiple operating systems generate different error messages
 	AssertEqual(t, strings.Split(soteErr.FmtErrMsg, ": open foo.txt:")[0], "209010: foo.txt file was not found. Message return")
@@ -98,7 +109,7 @@ func TestEmailAttachment(t *testing.T) {
 }
 
 func TestEmailValidateEmail(t *testing.T) {
-	email := NewEmail("staging", "Test Subject")
+	email := newTestEmail("staging", "Test Subject")
 	soteErr := email.isEmailValid("To", "us")
 	AssertEqual(t, soteErr.FmtErrMsg, "207050: us (To) is not a valid email address ERROR DETAILS: >>Key: EMAIL_LENGTH Value: mail: invalid string")
 	soteErr = email.isEmailValid("To", "us.com")
@@ -109,7 +120,7 @@ func TestEmailValidateEmail(t *testing.T) {
 
 func TestEmailAddAttachments(t *testing.T) {
 	var buffer bytes.Buffer
-	email := NewEmail("staging", "Test Subject")
+	email := newTestEmail("staging", "Test Subject")
 	email.attachments = append(email.attachments, attachment{
 		filepath:    "schema_test.json",
 		contentType: "text/plain; charset=utf-8",
@@ -127,20 +138,14 @@ SGVsbG8gV29ybGQ=`, " "))
 }
 
 func TestEmailInit(t *testing.T) {
-	email := NewEmail("staging", "Test Subject")
-	email.GetSmtpUsername = func(application, environment string) (string, sError.SoteError) {
-		return "USERNAME", sError.SoteError{}
-	}
-	email.GetSmtpPassword = func(application, environment string) (string, sError.SoteError) {
-		return "PASSWORD", sError.SoteError{}
-	}
+	email := newTestEmail("staging", "Test Subject")
 	auth, soteErr := email.initAuth()
 	AssertEqual(t, soteErr.FmtErrMsg, "")
 	AssertEqual(t, fmt.Sprintf("%v", auth), "&{ USERNAME PASSWORD email-smtp.eu-west-1.amazonaws.com}")
 }
 
 func TestEmailSendMail(t *testing.T) {
-	email := NewEmail("staging", "Test Subject")
+	email := newTestEmail("staging", "Test Subject")
 	email.To("toAdmin@getsote.com")
 	email.To("toSales@getsote.com").Name("To Customer Support")
 	email.Cc("ccAdmin@getsote.com")
@@ -202,34 +207,34 @@ SGVsbG8gV29ybGQ=
 }
 
 func TestEmailSendMailFailed(t *testing.T) {
-	email := NewEmail("staging", "Test Subject")
+	email := newTestEmail("staging", "Test Subject")
 	soteErr := email.Send("Hello World")
 	AssertEqual(t, soteErr.FmtErrMsg, "210599: Business Service error has occurred that is not expected. ERROR DETAILS: >>Key: SMTP_ERROR Value: 503 Error: need RCPT command")
 }
 
 func TestEmailSendMailInvalidFrom(t *testing.T) {
-	email := NewEmail("staging", "Test Subject")
+	email := newTestEmail("staging", "Test Subject")
 	email.from.address = "invalid.com"
 	soteErr := email.Send("Hello World")
 	AssertEqual(t, soteErr.FmtErrMsg, "207050: invalid.com (From) is not a valid email address ERROR DETAILS: >>Key: EMAIL_PARSE Value: mail: misformatted email address")
 }
 
 func TestEmailSendMailInvalidTo(t *testing.T) {
-	email := NewEmail("staging", "Test Subject")
+	email := newTestEmail("staging", "Test Subject")
 	email.To("invalid.com")
 	soteErr := email.Send("Hello World")
 	AssertEqual(t, soteErr.FmtErrMsg, "207050: invalid.com (To) is not a valid email address ERROR DETAILS: >>Key: EMAIL_PARSE Value: mail: misformatted email address")
 }
 
 func TestEmailSendMailInvalidCc(t *testing.T) {
-	email := NewEmail("staging", "Test Subject")
+	email := newTestEmail("staging", "Test Subject")
 	email.Cc("invalid.com")
 	soteErr := email.Send("Hello World")
 	AssertEqual(t, soteErr.FmtErrMsg, "207050: invalid.com (Cc) is not a valid email address ERROR DETAILS: >>Key: EMAIL_PARSE Value: mail: misformatted email address")
 }
 
 func TestEmailSendMailInvalidBcc(t *testing.T) {
-	email := NewEmail("staging", "Test Subject")
+	email := newTestEmail("staging", "Test Subject")
 	email.Bcc("invalid.com")
 	soteErr := email.Send("Hello World")
 	AssertEqual(t, soteErr.FmtErrMsg, "207050: invalid.com (Bcc) is not a valid email address ERROR DETAILS: >>Key: EMAIL_PARSE Value: mail: misformatted email address")
