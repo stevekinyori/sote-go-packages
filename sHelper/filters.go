fb0b22ff8d604e141150fc225a8682334a2ec37c
stephen morris kinyori <kinyorimorris@gmail.com>
2022-05-18 07:03:52 +0000

    Revert "Add AWS account-id ssm variable"

--- sHelper/filters.go
+++ sHelper/filters.go
@@ -1,5 +1,10 @@
 package sHelper
 
+import (
+	"gitlab.com/soteapps/packages/v2021/sLogger"
+	"reflect"
+)
+
 // FilterCommon describes the  format for common filter expression in the request json payload
 type FilterCommon struct {
 	Operator string      `json:"operator" mod:"ucase" validate:"required,oneof=> < = >= <= != <> IN 'NOT IN'"`
@@ -14,8 +19,20 @@ type FilterFields struct {
 
 // ShipmentFinTransFilterFields describes the  format for filter expression in the shipment fintrans request json payload
 type ShipmentFinTransFilterFields struct {
-	FieldName string `json:"field-name" validate:"required,oneof=shipments-id client-company-id fintrans-type currency-type transactions-amount transactions-timestamp created-by-requestor-username bill-of-lading memo cost-is-unexpected"`
 	FilterCommon
+	FieldName string `json:"field-name" validate:"required,oneof=shipments-id client-company-id fintrans-type currency-type transactions-amount transactions-timestamp created-by-requestor-username bill-of-lading memo cost-is-unexpected"`
+}
+
+// ShipmentsFilterFields describes the format for filter expression in the shipment's request json payload
+type ShipmentsFilterFields struct {
+	FilterCommon
+	FieldName string `json:"field-name" validate:"required,oneof=shipments-id row-version organizations-id client-company-id client-company-name suppliers-id supplier-name shipment-status bill-of-lading port-of-entry port-of-origin country-destination shipment-port-of-entry-eta consignment-wgt consignment-wgt-uom through-bill-of-lading created-by-date updated-by-date created-by-aws-user-name updated-by-aws-user-name"`
+}
+
+// ContainerFilterFields describes the format for filter expression in the container's request json payload
+type ContainerFilterFields struct {
+	FilterCommon
+	FieldName string `json:"field-name" validate:"required,oneof=containers-id organizations-id row-version container-tag container-form-factor shipments-id trips-id cargo-gross-wgt cargo-gross-uom created-by-date updated-by-date created-by-aws-user-name updated-by-aws-user-name"`
 }
 
 // ArrFilterParam filter parameters for a slice/array
@@ -24,4 +41,29 @@ type ArrFilterParam struct {
 	Prefix            string
 	InitialParamCount int
 	CaseInsensitive   bool
+}
+
+// SetFilters set convert filters of map[string][]ShipmentsFilterFields | map[string][] ContainerFilterFields to map[string][]FilterFields
+func SetFilters[F ShipmentsFilterFields | ContainerFilterFields | ShipmentFinTransFilterFields](tFilters map[string][]F) (filters map[string][]FilterFields) {
+	sLogger.DebugMethod()
+
+	if len(tFilters) > 0 {
+		filters = make(map[string][]FilterFields, 0)
+		for operand, filterValues := range tFilters {
+			for _, v := range filterValues {
+				field := FilterFields{}
+				f := reflect.ValueOf(v).Interface()
+				switch f.(type) {
+				case ShipmentsFilterFields:
+					field = FilterFields(f.(ShipmentsFilterFields))
+				case ContainerFilterFields:
+					field = FilterFields(f.(ContainerFilterFields))
+				case ShipmentFinTransFilterFields:
+					field = FilterFields(f.(ShipmentFinTransFilterFields))
+				}
+				filters[operand] = append(filters[operand], field)
+			}
+		}
+	}
+	return
 }
