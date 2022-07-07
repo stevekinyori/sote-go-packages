ab4183806112f345a625776802200714acdb0217
stephen morris kinyori <kinyorimorris@gmail.com>
2022-07-07 21:17:52 +0300

    add GetBusinessServiceIds to sMessage

---
+++ sMessage/constants.go
@@ -0,0 +1,39 @@
+package sMessage
+
+import (
+	"gitlab.com/soteapps/packages/v2021/sLogger"
+)
+
+const (
+	ORGGANIZATIONSBSID        = "5"
+	USERMANAGEMENTBSID        = "6"
+	SHIPMENTSBSID             = "7"
+	TRIPSBSID                 = "8"
+	SHIPMENTSFINTRANSBSID     = "9"
+	TRIPFINTRANSBSID          = "10"
+	DOCUMENTSBSID             = "11"
+	NOTESBSID                 = "12"
+	LISTOFVALUESBSID          = "13"
+	COGNITOBSID               = "14"
+	COGNITOUSERMANAGEMENTBSID = "15"
+	QUICKBOOKSBSID            = "16"
+)
+
+func GetBusinessServiceIds() []string {
+	sLogger.DebugMethod()
+
+	return []string{
+		ORGGANIZATIONSBSID,
+		USERMANAGEMENTBSID,
+		SHIPMENTSBSID,
+		TRIPSBSID,
+		SHIPMENTSFINTRANSBSID,
+		TRIPFINTRANSBSID,
+		DOCUMENTSBSID,
+		NOTESBSID,
+		LISTOFVALUESBSID,
+		COGNITOBSID,
+		COGNITOUSERMANAGEMENTBSID,
+		QUICKBOOKSBSID,
+	}
+}
